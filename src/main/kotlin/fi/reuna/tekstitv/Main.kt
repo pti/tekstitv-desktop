2476eb80e99a5111603491ef5b7529d538e3e798
Pekko Tiitto <tiittop@gmail.com>
2019-01-13 17:25:53 +0200

    Remember window position and size.

--- src/main/kotlin/fi/reuna/tekstitv/Main.kt
+++ src/main/kotlin/fi/reuna/tekstitv/Main.kt
@@ -2,9 +2,16 @@ package fi.reuna.tekstitv
 
 import java.awt.Color
 import java.awt.EventQueue
+import java.awt.event.WindowEvent
+import java.util.prefs.Preferences
 import javax.swing.JFrame
 
 private var controller: Controller? = null
+
+private const val PREF_WIN_X = "win_x"
+private const val PREF_WIN_Y = "win_y"
+private const val PREF_WIN_W = "win_w"
+private const val PREF_WIN_H = "win_h"
 
 fun main(args: Array<String>) {
     Log.debug("begin")
@@ -13,10 +20,17 @@ fun main(args: Array<String>) {
 //    System.setProperty("awt.useSystemAAFontSettings", "on")
 //    System.setProperty("prism.lcdtext", "false")
 
+    val prefs = Preferences.userNodeForPackage(Controller::class.java)
     val frame = JFrame("TekstiTV")
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     frame.background = Color.BLACK
-    frame.setBounds(220, 220, 500, 600) // TODO save and load size&pos from prefs
+
+    frame.setSize(prefs.getInt(PREF_WIN_W, 500), prefs.getInt(PREF_WIN_H, 600))
+
+    if (prefs.get(PREF_WIN_X, null) != null && prefs.get(PREF_WIN_W, null) != null) {
+        frame.setLocation(prefs.getInt(PREF_WIN_X, 0), prefs.getInt(PREF_WIN_Y, 0))
+    }
+
     frame.isVisible = true
 
     EventQueue.invokeLater {
@@ -24,4 +38,14 @@ fun main(args: Array<String>) {
         frame.contentPane.add(panel)
         controller = Controller(panel)
     }
+
+    frame.observeWindowEvents()
+            .filter { it.id == WindowEvent.WINDOW_CLOSING }
+            .subscribe {
+                prefs.putInt(PREF_WIN_X, frame.x)
+                prefs.putInt(PREF_WIN_Y, frame.y)
+                prefs.putInt(PREF_WIN_W, frame.width)
+                prefs.putInt(PREF_WIN_H, frame.height)
+                prefs.flush()
+            }
 }
--- src/main/kotlin/fi/reuna/tekstitv/RxSwing.kt
+++ src/main/kotlin/fi/reuna/tekstitv/RxSwing.kt
@@ -6,7 +6,10 @@ import java.awt.Component
 import java.awt.EventQueue
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
+import java.awt.event.WindowEvent
+import java.awt.event.WindowListener
 import java.util.concurrent.Executor
+import javax.swing.JFrame
 import javax.swing.SwingUtilities
 
 
@@ -14,24 +17,41 @@ fun Component.observeKeyEvents(): Observable<KeyEvent> = Observable.create<KeyEv
 
     val keyListener = object : KeyListener {
 
-        override fun keyPressed(e: KeyEvent?) {
-            emitter.onNext(e!!)
-        }
+        override fun keyPressed(e: KeyEvent?) { emitter.onNext(e!!) }
 
-        override fun keyReleased(e: KeyEvent?) {
-            emitter.onNext(e!!)
-        }
+        override fun keyReleased(e: KeyEvent?) { emitter.onNext(e!!) }
 
-        override fun keyTyped(e: KeyEvent?) {
-            emitter.onNext(e!!)
-        }
+        override fun keyTyped(e: KeyEvent?) { emitter.onNext(e!!) }
     }
 
     addKeyListener(keyListener)
     emitter.setCancellable { removeKeyListener(keyListener) }
 }
 
-fun <T> Observable<T>.observeOnEventQueue() = this!!.observeOn(eventQueueScheduler)!!
+fun JFrame.observeWindowEvents(): Observable<WindowEvent> = Observable.create<WindowEvent> { emitter ->
+
+    val windowListener = object : WindowListener {
+
+        override fun windowActivated(e: WindowEvent?) { emitter.onNext(e!!) }
+
+        override fun windowClosed(e: WindowEvent?) { emitter.onNext(e!!) }
+
+        override fun windowClosing(e: WindowEvent?) { emitter.onNext(e!!) }
+
+        override fun windowDeactivated(e: WindowEvent?) { emitter.onNext(e!!) }
+
+        override fun windowDeiconified(e: WindowEvent?) { emitter.onNext(e!!) }
+
+        override fun windowIconified(e: WindowEvent?) { emitter.onNext(e!!) }
+
+        override fun windowOpened(e: WindowEvent?) { emitter.onNext(e!!) }
+    }
+
+    addWindowListener(windowListener)
+    emitter.setCancellable { removeWindowListener(windowListener) }
+}
+
+fun <T> Observable<T>.observeOnEventQueue() = observeOn(eventQueueScheduler)!!
 
 private val eventQueueScheduler = Schedulers.from(SwingScheduler)
 
