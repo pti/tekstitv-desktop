a15605354d899a0a26ca4a132a3ac964d82f5eae
Pekko Tiitto <tiittop@gmail.com>
2019-06-26 22:13:22 +0300

    Removed setting property syncInterval as Preferences aren't used anymore.

--- src/main/kotlin/fi/reuna/tekstitv/Main.kt
+++ src/main/kotlin/fi/reuna/tekstitv/Main.kt
@@ -12,11 +12,6 @@ fun main() {
     // NavigationHistory can take a few tens of ms to load so do it while initializing the rest of the stuff.
     thread { NavigationHistory.instance }
 
-    // Preferences by default syncs every 30 seconds (at least on oracle 1.8.0_181).
-    // In this case it is completely unnecessary => increase the interval to a week.
-    // https://stackoverflow.com/questions/17376200/java-util-preferences-constantly-accesses-disk-about-every-30-secs
-    System.setProperty("java.util.prefs.syncInterval", "604800")
-
     EventQueue.invokeLater {
         val cfg = ConfigurationProvider.cfg
         val frame = JFrame("Teksti-TV")
